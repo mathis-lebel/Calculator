34579be0fbb78631b9e822b3e298195b17172fd5
0osmoz0 <skillpypy@gmail.com>
2025-12-25 19:12:12 +0100

    I finish the operation file with all operation

---
+++ src/Calculator/core/operations.py
@@ -0,0 +1,15 @@
+def add(x , y):
+    return x + y
+
+def sub(x ,y):
+    return x - y
+
+def div(x , y):
+    if y == 0:
+        return ValueError("Cannot divide by 0")
+    
+    return x / y
+
+def mul(x , y):
+    return x * y
+
